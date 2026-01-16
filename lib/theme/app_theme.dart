5e9fb58091966f1904d157ff9dca6bc34b3e1242
Miguel MA. Areal <miguel.areal04@gmail.com>
2026-01-16 11:00:25 +0000

    feat: nova fonte Audible
    - Implementa nova fonte
    - Altera o app_theme.dart e pubspec.yaml para acomodar nova fonte

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData get lightTheme => ThemeData(
-    useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.green,
-      brightness: Brightness.light,
-    ),
-    // Alterado de CardTheme para CardThemeData
-    cardTheme: CardThemeData(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-        side: BorderSide(color: Colors.grey.shade200),
-      ),
-    ),
-  );
+  static const String _fontFamily = 'AudibleSans';
 
-  static ThemeData get darkTheme => ThemeData(
-    useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.green,
-      brightness: Brightness.dark,
-    ),
-    cardTheme: CardThemeData(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-        side: BorderSide(color: Colors.grey.shade800),
+  static ThemeData get lightTheme => _createTheme(Brightness.light);
+  static ThemeData get darkTheme => _createTheme(Brightness.dark);
+
+  static ThemeData _createTheme(Brightness brightness) {
+    final isDark = brightness == Brightness.dark;
+
+    return ThemeData(
+      useMaterial3: true,
+      brightness: brightness,
+      fontFamily: _fontFamily,
+
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.green,
+        brightness: brightness,
       ),
-    ),
-  );
+
+      textTheme: const TextTheme(
+        displayLarge: TextStyle(fontWeight: FontWeight.w700),
+        bodyLarge: TextStyle(fontWeight: FontWeight.w400),
+      ),
+
+      cardTheme: CardThemeData(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: BorderSide(
+            color: isDark ? Colors.grey.shade800 : Colors.grey.shade200,
+          ),
+        ),
+      ),
+
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontFamily: _fontFamily,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
 }
